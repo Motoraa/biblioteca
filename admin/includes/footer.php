c74ee7e1c4cd8af2565e3d2500d1642bb8ddf168
Motoraa <72560939+Motoraa@users.noreply.github.com>
2022-03-28 16:29:01 +0300

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,12 @@
+   <section class="footer-section">
+   <center>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Biblioteca online 
+                </div>
+
+            </div>
+        </div>
+		</center>
+    </section>

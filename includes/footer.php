fad27ddcbaf45aef4bd65e8823883a1f7eb90796
Motoraa <72560939+Motoraa@users.noreply.github.com>
2022-03-28 16:30:21 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; Biblioteca online | 2021</a> 
+                   &copy; Biblioteca online | 2021
                 </div>
 
             </div>

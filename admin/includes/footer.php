ce6a4470535921a625a6f4ebe805ff9ebdb4de84
Motoraa <72560939+Motoraa@users.noreply.github.com>
2022-03-28 16:24:30 +0300

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; Biblioteca online |<a href="https://www.linkedin.com/in/aleksandart96/" target="_blank"  > Designed by : Motora Andrei</a> 
+                   &copy; Biblioteca online | Designed by : Motora Andrei</a> 
                 </div>
 
             </div>
